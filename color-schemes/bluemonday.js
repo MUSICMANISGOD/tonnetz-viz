33965ece93b0cb5d3a170d8b76528a9b507ab80d
MUSICMANISGOD <105236325+MUSICMANISGOD@users.noreply.github.com>
2022-05-11 13:32:30 -0400

    Add a new color scheme

---
+++ color-schemes/bluemonday.js
@@ -0,0 +1,38 @@
+colorscheme.addScheme("Bluemonday", {
+  "name": "Bluemonday",
+  "background": "#ffffff",
+  "nodes": {
+    "OFF": {
+      "label": "#aaaaaa",
+      "fill": "#000000",
+      "stroke": "#000000"
+    },
+    "GHOST": {
+      "label": "#ffffff",
+      "fill": "#ffffff",
+      "stroke": "#003375"
+    },
+    "SUSTAIN": {
+      "label": "#d1dbf0",
+      "fill": "#46629e",
+      "stroke": "#0e1f5b"
+    },
+    "ON": {
+      "label": "#ffffff",
+      "fill": "#00087a",
+      "stroke": "#0e1f5b"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#c0c0c0",
+      "label-on": "#bea866",
+      "fill": "#faf7db"
+    },
+    "minor": {
+      "label-off": "#c0c0c0",
+      "label-on": "#aa9b68",
+      "fill": "#eeebc9"
+    }
+  }
+});
