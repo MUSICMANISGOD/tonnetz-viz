2b7bcec34fc7fa61fd72e6084ecb858674be7d0d
MUSICMANISGOD <105236325+MUSICMANISGOD@users.noreply.github.com>
2022-05-12 12:38:11 -0400

    Create red.js

---
+++ color-schemes/red.js
@@ -0,0 +1,38 @@
+colorscheme.addScheme("red", {
+  "name": "red",
+  "background": "#000000",
+  "nodes": {
+    "OFF": {
+      "label": "#009e20",
+      "fill": "#00b803",
+      "stroke": "#00ff11"
+    },
+    "GHOST": {
+      "label": "#b75757",
+      "fill": "#850000",
+      "stroke": "#a80000"
+    },
+    "SUSTAIN": {
+      "label": "#b75757",
+      "fill": "#883535",
+      "stroke": "#b65454"
+    },
+    "ON": {
+      "label": "#ff8f8f",
+      "fill": "#ff0000",
+      "stroke": "#d34a4a"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#121212",
+      "label-on": "#525252",
+      "fill": "#212121"
+    },
+    "minor": {
+      "label-off": "#121212",
+      "label-on": "#6d6d6d",
+      "fill": "#161616"
+    }
+  }
+});
