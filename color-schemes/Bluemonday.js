d170fc08eab7e33f7d63dda9389d18b7eb5c5722
MUSICMANISGOD <105236325+MUSICMANISGOD@users.noreply.github.com>
2022-05-17 12:21:40 -0400

    Create Bluemonday.js

---
+++ color-schemes/Bluemonday.js
@@ -0,0 +1,38 @@
+{
+  "name": "Blue monday",
+  "background": "#000000",
+  "nodes": {
+    "OFF": {
+      "label": "#ffffff",
+      "fill": "#000000",
+      "stroke": "#0011ff"
+    },
+    "GHOST": {
+      "label": "#ffffff",
+      "fill": "#000cb3",
+      "stroke": "#0014a8"
+    },
+    "SUSTAIN": {
+      "label": "#d1dbf0",
+      "fill": "#4766a9",
+      "stroke": "#0e1f5b"
+    },
+    "ON": {
+      "label": "#ffffff",
+      "fill": "#0099cc",
+      "stroke": "#0e1f5b"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#c0c0c0",
+      "label-on": "#bea866",
+      "fill": "#00804d"
+    },
+    "minor": {
+      "label-off": "#c0c0c0",
+      "label-on": "#aa9b68",
+      "fill": "#000dc2"
+    }
+  }
+}
